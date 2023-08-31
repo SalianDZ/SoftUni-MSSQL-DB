477707a83b4d9e7a6216149cef2dfbba17cfc1dd
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-08-31 16:33:35 +0300

    Alter Table

---
+++ Databases Introduction/Alter Table.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE [Minions];
+
+USE	[Minions];
+
+CREATE TABLE [Minions] (
+	[Id] INT PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Age] INT NOT NULL
+)
+
+
+
+CREATE TABLE [Towns] (
+	[Id] INT PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+ALTER TABLE [Minions]
+ADD [TownId] INT FOREIGN KEY REFERENCES [Towns] ([Id]) NOT NULL
